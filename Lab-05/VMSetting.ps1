db93613b476660910431592a2c2d43139b654021
cemvarol <cem.varol@outlook.com>
2020-08-28 14:39:28 +0100

    Add files via upload

---
+++ Lab-05/VMSetting.ps1
@@ -0,0 +1,28 @@
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+Set-Service W32Time -StartupType Automatic
+w32tm /config /syncfromflags:manual /manualpeerlist:"time.windows.com"
+w32tm /config /reliable:yes
+net start w32time
+
+w32tm /resync /nowait
+
+Tzutil /s "GMT Standard Time"
+
+Set-NetFirewallProfile -Enabled False
+
+
+  
+$urlb = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
+$outputb = "$env:USERPROFILE\downloads\ChromeSetup.exe"
+Invoke-WebRequest -Uri $urlb -OutFile $outputb
+
+& "$env:USERPROFILE\downloads\ChromeSetup.exe"
+
+
+
