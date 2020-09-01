5f96fb458262cc7d0209304a2c5dbd1d96096ad4
cemvarol <cem.varol@outlook.com>
2020-09-01 13:56:00 +0100

    Update Set-Lab9.ps1

--- Lab-09/Set-Lab9.ps1
+++ Lab-09/Set-Lab9.ps1
@@ -2,8 +2,6 @@
 cd\
 mkdir Lab09
 
-
-
 $url = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/Set-Lab.ps1"
 $output = "C:\Lab09\Lab09.ps1"
 Invoke-WebRequest -Uri $url -OutFile $output
