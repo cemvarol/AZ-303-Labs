c5c2285b601cac995b52ce1fc805021a0563c798
cemvarol <cem.varol@outlook.com>
2020-09-01 13:42:01 +0100

    Update Set-Lab9.ps1

--- Lab-09/Set-Lab9.ps1
+++ Lab-09/Set-Lab9.ps1
@@ -1,6 +1,6 @@
 cd\
 mkdir L9
 
-$url = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/SetHost.ps1"
+$url = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/Set-Lab.ps1"
 $output = "C:\L9\L09.ps"
 Invoke-WebRequest -Uri $url -OutFile $output1
