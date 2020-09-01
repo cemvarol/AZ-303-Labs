62d1ce7d71e33bcd28c80d8604d74e20add33fd3
cemvarol <cem.varol@outlook.com>
2020-09-01 13:42:54 +0100

    Update Set-Lab9.ps1

--- Lab-09/Set-Lab9.ps1
+++ Lab-09/Set-Lab9.ps1
@@ -2,5 +2,8 @@ cd\
 mkdir L9
 
 $url = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/Set-Lab.ps1"
-$output = "C:\L9\L09.ps"
+$output = "C:\L9\L09.ps1"
 Invoke-WebRequest -Uri $url -OutFile $output1
+
+
+Start-Process Powershell.exe -Argumentlist "-file C:\L09\L09.ps1"
