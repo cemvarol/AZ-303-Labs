1b01d836f540f2e04518be54abb8541a09a6d98c
cemvarol <cem.varol@outlook.com>
2020-09-30 12:04:40 +0100

    Update Lab-07-Users.bash

--- Lab-07/Lab-07-Users.bash
+++ Lab-07/Lab-07-Users.bash
@@ -1,6 +1,12 @@
-A=$(az ad signed-in-user show --query userPrincipalName)
+# Get Login Name
+a=$(az ad signed-in-user show --query userPrincipalName)
+# Set change Login Name to lower-case
+A=$(echo "$a" | sed -e 's/\(.*\)/\L\1/')
+# Remove the part after @ sign
 B=${A:$(echo `expr index "$A" @`)}
+# Remove quotaion marks
 C=${B:: -1}
+
 az ad user create --display-name "StorageAccount Contributor" --user-principal-name sacc.cont@$C --password 1q2w3e4r5t6y*
 az ad user create --display-name "Network Contributor" --user-principal-name nw.cont@$C --password 1q2w3e4r5t6y*
 az ad user create --display-name "VM Contributor" --user-principal-name vm.cont@$C --password 1q2w3e4r5t6y*
