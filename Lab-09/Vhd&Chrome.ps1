9a4ee68dcb96ac28ac73ab96fa488d1e13be9720
cemvarol <cem.varol@outlook.com>
2021-03-18 07:52:58 +0000

    Update Vhd&Chrome.ps1

--- Lab-09/Vhd&Chrome.ps1
+++ Lab-09/Vhd&Chrome.ps1
@@ -8,5 +8,5 @@ Start-Sleep -s 60
 
 & "C:\Program Files\google\chrome\Application\chrome.exe" http://download.microsoft.com/download/5/8/1/58147EF7-5E3C-4107-B7FE-F296B05F435F/9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd
 
-Start-Sleep -s 240
+Start-Sleep -s 270
 Start-Process Powershell.exe -Argumentlist "-file C:\SC\3-CreateVMs.ps1"
